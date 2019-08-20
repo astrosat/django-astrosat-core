d6f6e21b91bfa2909c94ebc73a60501bd43f8a9f
Allyn Treshansky <allyn.treshansky@gmail.com>
2019-08-20 12:36:07 +0100

    conditional decorator

--- astrosat/utils/__init__.py
+++ astrosat/utils/__init__.py
@@ -3,6 +3,7 @@
 from .utils_dynamic_settings import DynamicSetting, DynamicAppSettings
 
 from .utils_data_client import DataClient
+from .utils_decorators import conditional_redirect
 from .utils_gis import adapt_geojson_to_django
 from .utils_profile import profile, track_memory
 from .utils_validators import validate_no_spaces, validate_no_tags, validate_schema
---
+++ astrosat/utils/utils_decorators.py
@@ -0,0 +1,29 @@
+import functools
+
+from django.shortcuts import redirect
+
+
+def conditional_redirect(conditional, redirect_name):
+
+    """
+    A decorator for a view that redirects to "redirect" if "conditional" is true
+    Parameters
+    ----------
+    conditional : fn
+        the function to check if a redirect will ocurr
+    redirect_name: fn
+        the view name to redirect to
+    """
+
+    def conditional_redirect_decorator(view_fn):
+
+        @functools.wraps(view_fn)
+        def conditional_redirect_wrapper(request, *args, **kwargs):
+
+            if conditional():
+                return redirect(redirect_name, *args, **kwargs)
+            return view_fn(request, *args, **kwargs)
+
+        return conditional_redirect_wrapper
+
+    return conditional_redirect_decorator
