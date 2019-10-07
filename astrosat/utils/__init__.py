a35201fea19e203d7df0f03fb64dca9f351344b6
Allyn Treshansky <allyn.treshansky@gmail.com>
2019-10-07 14:16:25 +0100

    feat(backend): added utils for iterating

--- astrosat/utils/__init__.py
+++ astrosat/utils/__init__.py
@@ -5,6 +5,7 @@ from .utils_dynamic_settings import DynamicSetting, DynamicAppSettings
 from .utils_data_client import DataClient
 from .utils_db import bulk_update_or_create
 from .utils_gis import adapt_geojson_to_django
+from .utils_iterators import partition
 from .utils_logging import RestrictLogsByNameFilter
 from .utils_profile import profile, track_memory
 from .utils_validators import validate_no_spaces, validate_no_tags, validate_schema
---
+++ astrosat/utils/utils_iterators.py
@@ -0,0 +1,26 @@
+from itertools import filterfalse, islice, tee, zip_longest
+
+
+#############
+# iterators #
+#############
+
+
+def grouper(iterable, n, fillvalue=None):
+    """
+    Collect data into fixed-length chunks or blocks"
+    from: https://docs.python.org/3/library/itertools.html#itertools-recipes
+    """
+    # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
+    args = [iter(iterable)] * n
+    return zip_longest(*args, fillvalue=fillvalue)
+
+
+def partition(pred, iterable):
+    """
+    Use a predicate to partition entries into false entries and true entries
+    from: https://docs.python.org/dev/library/itertools.html#itertools-recipes
+    """
+    # partition(is_odd, range(10)) --> 0 2 4 6 8   and  1 3 5 7 9
+    t1, t2 = tee(iterable)
+    return filterfalse(pred, t1), filter(pred, t2)
