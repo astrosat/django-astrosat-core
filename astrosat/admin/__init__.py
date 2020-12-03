a1f7b17de95e86dcb996a296bcfa14a10103752a
Allyn Treshansky <allyn.treshansky@gmail.com>
2020-12-03 12:23:58 +0000

    feat(backend): refactored admin
    Refactored the admin code into separate modules as it was getting kind
    of large w/ lots of unrelated sections in the single module.

---
+++ astrosat/admin/__init__.py
@@ -0,0 +1,16 @@
+from .admin_base import (
+    CannotAddModelAdminBase,
+    CannotDeleteModelAdminBase,
+    CannotUpdateModelAdminBase,
+    CannotEditModelAdminBase,
+    ReadOnlyModelAdminBase,
+    DeleteOnlyModelAdminBase
+)
+from .admin_utils import (
+    IncludeExcludeListFilter,
+    JSONAdminWidget,
+    get_clickable_m2m_list_display,
+    get_clickable_fk_list_display,
+)
+from .admin_logging import *
+from .admin_settings import *
